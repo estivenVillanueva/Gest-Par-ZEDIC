cd15fcf5eeb2cdf3894d8534651822add10ffb57
estivenVillanueva <estivenVillanueva>
2025-06-11 09:21:28 -0500

    solucion al error de importacion

--- frontend/src/pages/vehiculo/MisVehiculos.jsx
+++ frontend/src/pages/vehiculo/MisVehiculos.jsx
@@ -22,7 +22,7 @@ import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
-import { useAuth } from '../../logic/AuthContext';
+import { useAuth } from '../../../logic/AuthContext';
 
 const MOCK_VEHICULOS = [
   {
